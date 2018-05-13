1205fff2a10c74178c137a603c393ab9116420b2
Juan Carlos Pérez <juan.carlos@wizeline.com>
2018-05-12 23:12:36 -0500

    Zip content of the file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 echo "###### Starting Deployment ######"
 ls -all
+echo "###### Installing modules ######"
 npm install
+echo "###### Installing Typescript ######"
 npm install -g typescript
+echo "###### Compiling Typescript ######"
 tsc
+echo "###### Removing dev dependencies ######"
+npm prune --production
+echo "###### Zip the package ######"
+zip -r Lambda.zip .  -i \"*.js\" \"*.json\"
+echo "###### Upload to S3 ######"
+ls -all
