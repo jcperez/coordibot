fdafc699dc2daa18565774a12302f079d53a1967
Juan Carlos Pérez <juan.carlos@wizeline.com>
2018-05-23 09:50:54 -0500

    Deploy to all lambda functions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,4 +15,6 @@ echo "###### List files ######"
 ls -all
 echo "###### Deploy to lambda ######"
 aws lambda update-function-code --function-name RetrieveAvailability --zip-file fileb://Lambda.zip > /dev/null
+aws lambda update-function-code --function-name RetrieveStats --zip-file fileb://Lambda.zip > /dev/null
+aws lambda update-function-code --function-name TestConnection --zip-file fileb://Lambda.zip > /dev/null
 
