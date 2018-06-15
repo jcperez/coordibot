5b8cee8ccc734314b882cb35ca2a7ecf777b2df6
Juan Carlos Pérez <juan.carlos@wizeline.com>
2018-06-14 17:32:48 -0700

    Fixed dates in mocked data

--- src/adapters/__mocks__/gcalendar.ts
+++ src/adapters/__mocks__/gcalendar.ts
@@ -19,16 +19,16 @@ interface RetrieveEventsOptions {
 const retrieveUserEvents = async (user: string, options: RetrieveEventsOptions): Promise<UserEvent[]> => {
   return Promise.resolve([
     {
-      start: `${moment().format('YYYY-MM-DD')}T14:00:00-05:00`,
-      end: `${moment().format('YYYY-MM-DD')}T15:00:00-05:00`,
+      start: `2018-05-25T14:00:00-05:00`,
+      end: `2018-05-25T15:00:00-05:00`,
       summary: 'Hold for interview',
       calendarId: 'juan.carlos@wizeline.com',
       warnings: [],
       creator: 'juan.carlos@wizeline.com'
     },
     {
-      start: `${moment().format('YYYY-MM-DD')}T10:00:00-05:00`,
-      end: `${moment().format('YYYY-MM-DD')}T12:00:00-05:00`,
+      start: `2018-05-25T10:00:00-05:00`,
+      end: `2018-05-25T12:00:00-05:00`,
       summary: 'Hold for interview',
       calendarId: 'juan.carlos@wizeline.com',
       warnings: [],
